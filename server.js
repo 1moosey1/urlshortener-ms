4cb9fb51e5196a4a0240e3b44b36f1ae7e34ebba
Moosey <jgonzalez498@gmail.com>
2017-07-21 15:31:36 -0700

    Added basic setup for get, post and index html/css

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/", function(request, response) {
+
+    response.send(request.headers);
+});
+
+app.get("/:urlid", function(request, response) {
+
+    response.send(request.params);
+});
+
 app.listen(process.env.PORT || 1337);
